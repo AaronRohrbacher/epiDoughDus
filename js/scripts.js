8eec246a1d262554e431845259b66e0ec6191dcb
Aaron Rohrbacher <arjazz@icloud.com>
2017-08-25 10:22:11 -0700

    pass fifth specification allowing user to select multiple toppings before completing pizza

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,21 @@ function PizzaCreator (pizza, size, toppings) {
   this.size = size;
   this.toppings = toppings;
 }
+var toppings = [];
 
 //UI logic
 $(document).ready(function() {
+  //add toppings to empty array for later construction in object
+  $("#addToppingButton").click(function() {
+    toppings.push(" " + $("#toppingsInput").val());
+    $("#addedToppings").text(toppings);
+  });
+  //create new pizza object and output it
   $("#pizzaForm").submit(function(event) {
     event.preventDefault();
+    console.log(toppings);
     var userInput = $("#pizzaInput").val();
     var sizeInput = $("#sizeInput").val();
-    var toppings = $("#toppingsInput").val()
     var newPizza = new PizzaCreator(userInput, sizeInput, toppings);
     $(".pizzaOutput").text(newPizza.size + " " + newPizza.pizza + " with " + newPizza.toppings);
   });
