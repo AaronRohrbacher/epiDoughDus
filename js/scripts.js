44dcb7feca555badc8d67d816536cfb2ff745c35
Aaron Rohrbacher <arjazz@icloud.com>
2017-08-25 11:01:46 -0700

    passsixth specification, displaying a price with each item ordered

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
 //Business logic
-function PizzaCreator (pizza, size, toppings) {
+var pizzaPrice = {item:["Large", "Medium", "Small", "Pepperoni", "Sausage", "Mushrooms"], price: [7.99, 5, 3, .5, 1, .75]}
+
+function PizzaCreator (pizza, size, toppings, price) {
   this.pizza = pizza;
   this.size = size;
   this.toppings = toppings;
+  this.price = price;
 }
 var toppings = [];
 
@@ -10,16 +13,20 @@ var toppings = [];
 $(document).ready(function() {
   //add toppings to empty array for later construction in object
   $("#addToppingButton").click(function() {
-    toppings.push(" " + $("#toppingsInput").val());
+    var topping = pizzaPrice.item[parseInt($("#toppingsInput").val())];
+    var price = pizzaPrice.price[parseInt($("#toppingsInput").val())].toFixed(2);
+    toppings.push(" " + topping + " (" + price + ")")
+    console.log(topping + price);
     $("#addedToppings").text(toppings);
   });
-  //create new pizza object and output it
+  //construct new pizza object and output it
   $("#pizzaForm").submit(function(event) {
     event.preventDefault();
     console.log(toppings);
     var userInput = $("#pizzaInput").val();
-    var sizeInput = $("#sizeInput").val();
+    var sizeInput = pizzaPrice.item[$("#sizeInput").val()];
+    var sizePrice = pizzaPrice.price[$("#sizeInput").val()].toFixed(2);
     var newPizza = new PizzaCreator(userInput, sizeInput, toppings);
-    $(".pizzaOutput").text(newPizza.size + " " + newPizza.pizza + " with " + newPizza.toppings);
+    $(".pizzaOutput").text(newPizza.size + " " + newPizza.pizza + " (" + sizePrice + ")  with " + newPizza.toppings);
   });
 });
