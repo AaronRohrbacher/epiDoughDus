4f223b5c72eb8d4cdb6e8297e53e5cbc5cd3e97d
Aaron Rohrbacher <arjazz@icloud.com>
2017-08-30 09:10:44 -0700

    add ability to total and display toppings via method

--- js/scripts.js
+++ js/scripts.js
@@ -8,13 +8,12 @@ function PizzaCreator (size, toppings, total) {
   this.total = total;
 };
 
-// PizzaCreator.prototype.addToppings = function(toppings) {
-//   this.total = 0;
-//   for (var i=0; i < toppings.length; ++i) {
-//     this.total += parseFloat(pointOfSale.itemPrice[i]);
-//   }
-//   return this.total;
-// };
+PizzaCreator.prototype.addToppings = function(toppings, pointOfSale) {
+  for (var i=0; i < toppings.length; ++i) {
+    this.total += pointOfSale.price[toppings[i]];
+    this.size += (" topping " + (i+1) + ": " + pointOfSale.item[toppings[i]]);
+  }
+};
 
 function PointOfSale() {
   this.item = ["Large Pizza", "Medium Pizza", "Small Pizza", "Pepperoni", "Sausage", "Mushrooms"];
@@ -24,12 +23,12 @@ function PointOfSale() {
 }
 // var pointOfSale = {item:["Large", "Medium", "Small", "Pepperoni", "Sausage", "Mushrooms"], price: [7.99, 5, 3, .5, 1, .75], displayItem: [], itemPrice: [], cart: []};
 
-PointOfSale.prototype.total = function() {
-  var total = 0;
-  for (var i = 0; i < pointOfSale.cart.length; ++i) {
-    total += parseFloat(pointOfSale.cart[i]);
-  }
-};
+// PointOfSale.prototype.total = function() {
+//   var total = 0;
+//   for (var i = 0; i < pointOfSale.cart.length; ++i) {
+//     total += parseFloat(pointOfSale.cart[i]);
+//   }
+// };
 
 PointOfSale.prototype.toCart = function (cartItem, itemPrice) {
   this.cart.push(cartItem);
@@ -58,15 +57,20 @@ $(document).ready(function() {
     $(".viewCart").empty();
     $("#addedToppings").empty();
     event.preventDefault();
+
+    //create pizza
     var itemIndexInput = parseInt($("#sizeInput").val());
     var newPizza = new PizzaCreator(newPointOfSale.item[itemIndexInput], toppings, newPointOfSale.price[itemIndexInput]);
+
+    //add toppings to PizzaCreator
+    newPizza.addToppings(toppings, newPointOfSale);
+
+    //push to cart
     newPointOfSale.toCart(newPizza.size, parseFloat(newPizza.total));
     $.each(newPointOfSale.cart, function(index, value) {
       $(".viewCart").append("<li>" + newPointOfSale.cart[index] + " " + newPointOfSale.cartItemPrice[index] + "</li>");
     });
-    console.log(newPizza);
-    console.log(toppings);
-    console.log(newPointOfSale.cart);
+    //reset toppings array for next pizza item
     toppings = [];
   });
   $("#addToppingButton").click(function() {
@@ -76,17 +80,5 @@ $(document).ready(function() {
     $.each(toppings, function (index, value) {
       $("#addedToppings").append(newPointOfSale.item[value] + " ")
     });
+  });
 });
-
-});
-  //add toppings to empty array for later construction in object
-  // $("#addToppingButton").click(function() {
-  //   var topping = pointOfSale.item[parseInt($("#toppingsInput").val())];
-  //   var toppingPrice = pointOfSale.price[parseInt($("#toppingsInput").val())];
-  //   pointOfSale.displayItem.push(" " + topping + " (add " + toppingPrice + ")");
-  //   pointOfSale.itemPrice.push(toppingPrice);
-  //   $("#addedToppings").text(pointOfSale.displayItem);
-  // });
-
-
-// });
