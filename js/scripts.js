b21cb846529f3ef76ae1f01c8df9fccb4a232ddb
Aaron Rohrbacher <aaronrohrbacher@Isaac.local>
2017-08-29 23:33:57 -0700

    display size and price using only objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,69 +1,84 @@
 //Business logic
-var cart = [];
 
-var pizzaMenu = {item:["Large", "Medium", "Small", "Pepperoni", "Sausage", "Mushrooms"], price: [7.99, 5, 3, .5, 1, .75], displayItem: [], itemPrice: []};
 
-function PizzaCreator (pizza, size, toppings, total) {
-  this.pizza = pizza;
+function PizzaCreator (size, total) {
+  // this.pizza = pizza;
   this.size = size;
-  this.toppings = toppings;
+  this.toppings = [];
   this.total = total;
-}
+};
 
 PizzaCreator.prototype.price = function() {
   var total = 0;
-  for (var i=0; i < pizzaMenu.itemPrice.length; ++i) {
-    total += parseFloat(pizzaMenu.itemPrice[i]);
+  for (var i=0; i < pointOfSale.itemPrice.length; ++i) {
+    total += parseFloat(pointOfSale.itemPrice[i]);
   }
   return total;
 };
 
-function toCart (cartItem) {
-  cart.push(cartItem);
-  pizzaMenu.displayItem = [];
-  pizzaMenu.itemPrice = [];
+function PointOfSale() {
+  this.item = ["Large Pizza", "Medium Pizza", "Small Pizza", "Pepperoni", "Sausage", "Mushrooms"];
+  this.price = [12, 7, 5, .5, 1, .75];
+  this.cartItemPrice = []
+  this.cart = [];
+}
+// var pointOfSale = {item:["Large", "Medium", "Small", "Pepperoni", "Sausage", "Mushrooms"], price: [7.99, 5, 3, .5, 1, .75], displayItem: [], itemPrice: [], cart: []};
+
+PointOfSale.prototype.total = function() {
+  var total = 0;
+  for (var i = 0; i < pointOfSale.cart.length; ++i) {
+    total += parseFloat(pointOfSale.cart[i]);
+  }
+};
+
+PointOfSale.prototype.toCart = function (cartItem, itemPrice) {
+  this.cart.push(cartItem);
+  this.cartItemPrice.push(itemPrice)
 }
 
-function cartTotal(fullCart) {
-  var total = 0;
-  for (var i=0; i < fullCart.length; ++i) {
-    total += parseFloat(fullCart[i].total);
-  }
-  return total;
-}
+// function toCart (cartItem) {
+//   cart.push(cartItem);
+//   pointOfSale.displayItem = [];
+//   pointOfSale.itemPrice = [];
+// }
+//
+// function cartTotal(fullCart) {
+//   var total = 0;
+//   for (var i=0; i < fullCart.length; ++i) {
+//     total += parseFloat(fullCart[i].total);
+//   }
+//   return total;
+// }
 
 //UI logic
 $(document).ready(function() {
+  var newPointOfSale = new PointOfSale();
+    $("#addToppingButton").click(function() {
+      var newTopping = parseInt($("#toppingsInput").val())
+      console.log(newTopping);
+  });
 
-  //construct new pizza object and output it
-  $("#pizzaForm").submit(function(event) {
-
+  $("#addToCart").click(function() {
     $(".viewCart").empty();
     event.preventDefault();
-
-    var pizzaInput = $("#pizzaInput").val();
-    var sizePrice = pizzaMenu.price[$("#sizeInput").val()];
-    var sizeInput = pizzaMenu.item[$("#sizeInput").val()] + " " + sizePrice;
-    pizzaMenu.itemPrice.push(sizePrice);
-    var newPizza = new PizzaCreator(pizzaInput, sizeInput, pizzaMenu.displayItem);
-    var pizzaTotal = newPizza.price();
-    newPizza.total = pizzaTotal;
-    toCart(newPizza);
-    $.each(cart, function(i, val) {
-      $(".viewCart").append("<li>" + cart[i].size + " " + cart[i].pizza + " " + cart[i].toppings.join() + " <strong>" + cart[i].total +"</strong></li>");
+    var itemIndexInput = parseInt($("#sizeInput").val());
+    var newPizza = new PizzaCreator(newPointOfSale.item[itemIndexInput],newPointOfSale.price[itemIndexInput]);
+    newPointOfSale.toCart(newPizza.size, parseFloat(newPizza.total));
+    console.log(newPointOfSale.cart);
+    console.log(newPointOfSale.cartItemPrice);
+    $.each(newPointOfSale.cart, function(index, value) {
+      $(".viewCart").append("<li>" + newPointOfSale.cart[index] + " " + newPointOfSale.cartItemPrice[index] + "</li>");
     });
-    var orderTotal = cartTotal(cart).toFixed(2);
-    $("#orderTotal").text("Order Total: " + orderTotal);
-    $("#addedToppings").empty();
   });
-  //add toppings to empty array for later construction in object
-  $("#addToppingButton").click(function() {
-    var topping = pizzaMenu.item[parseInt($("#toppingsInput").val())];
-    var toppingPrice = pizzaMenu.price[parseInt($("#toppingsInput").val())];
-    pizzaMenu.displayItem.push(" " + topping + " (add " + toppingPrice + ")");
-    pizzaMenu.itemPrice.push(toppingPrice);
-    $("#addedToppings").text(pizzaMenu.displayItem);
-  });
-
-
 });
+  //add toppings to empty array for later construction in object
+  // $("#addToppingButton").click(function() {
+  //   var topping = pointOfSale.item[parseInt($("#toppingsInput").val())];
+  //   var toppingPrice = pointOfSale.price[parseInt($("#toppingsInput").val())];
+  //   pointOfSale.displayItem.push(" " + topping + " (add " + toppingPrice + ")");
+  //   pointOfSale.itemPrice.push(toppingPrice);
+  //   $("#addedToppings").text(pointOfSale.displayItem);
+  // });
+
+
+// });
